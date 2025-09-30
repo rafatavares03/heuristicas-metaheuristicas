ccae261311dae575b6b8492cd8b7e1291fbc2482
Rafael Tavares <rafatavares03@hotmail.com>
2025-09-30 09:12:24 -0300

    SA

---
+++ simulatedAnnealing/SA_TSP.cpp
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+vector<vector<int>> grafo;
+vector<vector<int>> coordenadas;
+
+int distancia(int i, int j) {
+  int xd = coordenadas[i][1] - coordenadas[j][1]; 
+  int yd = coordenadas[i][2] - coordenadas[j][2]; 
+  return (int)(sqrt(xd*xd + yd*yd) + 0.5);
+}
+
+vector<vector<int>> matriz() {
+  vector<vector<int>> g(coordenadas.size(), vector<int>(coordenadas.size(), -1));
+  for(int i = 0; i < coordenadas.size(); i++) g[i][i] = 0;
+  for(int i = 0; i < coordenadas.size(); i++) {
+    for(int j = i+1; j < coordenadas.size(); j++){
+      int peso = distancia(i, j);
+      g[i][j] = peso;
+      g[j][i] = peso;
+    }
+  }
+  return g;
+}
+
+// Guloso: vizinhos mais próximos
+vector<int> solucaoInicial() {
+  vector<int> caminho;
+  vector<bool> vertices(grafo.size(), false);
+  for(int i = 1; i < grafo.size(); i++) vertices.push_back(i);
+  caminho.push_back(0);
+  vertices[0] = true;
+  while(caminho.size() < grafo.size()) {
+    int menorDistancia = 0x3f3f3f, v = caminho.size() - 1, u = -1;
+    for(int i = 0; i < grafo.size(); i++) {
+      if(grafo[caminho[v]][i] < menorDistancia && !vertices[i]) {
+        menorDistancia = grafo[caminho[v]][i];
+        u = i;
+      }
+    }
+    caminho.push_back(u);
+    vertices[u] = true;
+  }
+  return caminho;
+}
+
+int calculaCusto(vector<int> caminho) {
+  if(caminho.empty()) return 0;
+  int custo = 0;
+  for(int i = 0; i < caminho.size()-1; i++) {
+    custo += grafo[caminho[i]][caminho[i+1]];
+  }
+  custo += grafo[caminho[0]][caminho[caminho.size()-1]];
+  return custo;
+}
+
+vector<int> swap(vector<int> array, int i, int j) {
+  vector<int> aux(array);
+  int x = aux[i];
+  aux[i] = aux[j];
+  aux[j] = x;
+  return aux;
+}
+
+vector<int> buscaLocal(vector<int> solucao) {
+  int custo = calculaCusto(solucao);
+  bool earlyStop = false;
+  int it = 0;
+  while(it < 1e6 && !earlyStop) {
+    int i = 0;
+    int custoInicial = custo;
+    while(i < solucao.size()) {
+      vector<int> solucaoAux(solucao);
+      for(int j = i+1; j < solucao.size(); j++) {
+        solucaoAux = swap(solucaoAux, i, j);
+        int custoAux = calculaCusto(solucaoAux);
+        if(custoAux < custo) {
+          solucao = swap(solucao,i,j);
+          custo = custoAux;
+          i = solucao.size();
+          j = solucao.size();
+        } else{
+          solucaoAux = swap(solucaoAux, i, j);
+        }
+      }
+      i++;
+    }
+    if(custoInicial == custo) {
+      earlyStop = true;
+    }
+    it++;
+  }
+  return solucao;
+}
+
+double probabilidade(int custo, int novoCusto, int t) {
+  double expoente = (double)(custo - novoCusto) / t;
+  return 1 / (1 + exp(expoente));
+}
+
+vector<int> simulatedAnnealing() {
+  vector<int> solucao = solucaoInicial();
+  vector<int> melhorSolucao;
+  double t = 1e9;
+  srand(time(0));
+  while(t > 0.01) {
+    int i = 0;
+    while(i < 10) {
+      int v = rand() % solucao.size();
+      int u = rand() % solucao.size();
+      if(v == u) v = (v+1) % solucao.size();
+      vector<int> aux = swap(solucao, u, v);
+      
+      int custoAux = calculaCusto(aux);
+      int custoSol = calculaCusto(solucao);
+
+      if(custoAux > custoSol) {
+        solucao = aux;
+        if(melhorSolucao.empty() || custoAux < calculaCusto(melhorSolucao)) {
+          cout << "entrou" << endl;
+          melhorSolucao = solucao;
+        }
+      } else if (rand() % 2 < exp((custoAux - custoSol)/t)) {
+        solucao = aux;
+      }
+
+      i++;
+    }
+    t = 0.85 * t;
+  }
+  return melhorSolucao;
+}
+
+int main() {
+  int v, x, y;
+  while(cin >> v >> x >> y) {
+    coordenadas.push_back({v,x,y});
+  }
+  grafo = matriz();
+  vector<int> caminho = simulatedAnnealing();
+  cout << "Solução: ";
+  for(int i = 0; i < caminho.size(); i++) cout << caminho[i] << " ";
+  cout << caminho[0] << endl;
+  cout << "Custo: " << calculaCusto(caminho) << endl;
+  return 0;
+}
